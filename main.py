4da310306351823c3508c42940893a0c1515e969
Jeka <jeka@email.com>
2026-08-16 16:08:13 +0300

    add csrf protect

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from flask_wtf.csrf import CSRFProtect
 
 from views.shifts import shifts_app
 
 app = Flask(__name__)
 
 app.register_blueprint(shifts_app, url_prefix="/shifts")
+
+csrf = CSRFProtect(app)
 
 
 @app.get("/", endpoint="index")
