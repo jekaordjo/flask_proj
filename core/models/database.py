573518ac911888e750a8482a2ee1e074bb44dac4
Jeka <jeka@email.com>
2026-08-16 17:14:14 +0300

    add db and migrate

---
+++ core/models/database.py
@@ -0,0 +1,14 @@
+__all__ = (
+    "Base",
+    "db",
+)
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template
+from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 
+from core.models import db
 from views.shifts import shifts_app
 
 app = Flask(__name__)
+# python -c 'import secrets; print(secrets.token_hex())'
+app.config.update(
+    SECRET_KEY="93d0bcf0bce0df9d694c958566690c66df71b8ffbb3aa591ce87c0df099275d5",
+    SQLALCHEMY_DATABASE_URI="sqlite:///app.db",
+)
 
 app.register_blueprint(shifts_app, url_prefix="/shifts")
 
 csrf = CSRFProtect(app)
+db.init_app(app)
+migrate = Migrate(app, db)
 
 
 @app.get("/", endpoint="index")
